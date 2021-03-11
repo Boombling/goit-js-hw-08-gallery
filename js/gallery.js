bdee2cd3c52e40c7379263890871cbef49531cf6
Dmytro Filius <mindertan@gmail.com>
2021-03-11 22:44:19 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -5,30 +5,17 @@ refs.galleryEl.insertAdjacentHTML("beforeend", createGalleryList(image));
 function createGalleryList(image) {
   return image
     .map((img) => `<li class="gallery__item">
-      < a
-    class= "gallery__link"
-    href = "${img.original}"
+      <a
+    class="gallery__link"
+    href ="${img.original}"
       >
       <img
         class="gallery__image"
         src= "${img.preview}"
-        data-source= "${img.original}"
-        alt= "${img.description}"
+        data-source="${img.original}"
+        alt="${img.description}"
       />
   </a >
 </li > `)
         .join("");
     }
-/* <li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-  >
-    <img
-      class="gallery__image"
-      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-      alt="Tulips"
-    />
-  </a>
-</li> */
