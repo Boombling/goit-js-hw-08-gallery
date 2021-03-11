ce427439fb445e42622109e63eca23e61e7d7917
Dmytro Filius <mindertan@gmail.com>
2021-03-11 22:36:55 +0200

    add gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,25 @@
-import image from './gallery-items.js'
+import image from './gallery-items.js';
+import { refs } from './refs.js';
 
-{/* <li class="gallery__item">
+refs.galleryEl.insertAdjacentHTML("beforeend", createGalleryList(image));
+function createGalleryList(image) {
+  return image
+    .map((img) => `<li class="gallery__item">
+      < a
+    class= "gallery__link"
+    href = "${img.original}"
+      >
+      <img
+        class="gallery__image"
+        src= "${img.preview}"
+        data-source= "${img.original}"
+        alt= "${img.description}"
+      />
+  </a >
+</li > `)
+        .join("");
+    }
+/* <li class="gallery__item">
   <a
     class="gallery__link"
     href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
@@ -12,4 +31,4 @@ import image from './gallery-items.js'
       alt="Tulips"
     />
   </a>
-</li> */}
+</li> */
