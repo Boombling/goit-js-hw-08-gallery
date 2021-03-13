ccb4b4947384743a5df021115e05222ade6ff5a6
Dmytro Filius <mindertan@gmail.com>
2021-03-14 00:24:04 +0200

    add modal window

--- js/work.js
+++ js/work.js
@@ -1,54 +1,57 @@
 import image from './gallery-items.js';
 import { refs } from './refs.js';
 
-refs.galleryEl.addEventListener('click', onOpenOriginalImage)
-refs.closeModalBtn.addEventListener('click', closeImageModal)
+refs.galleryEl.addEventListener('click', onOpenModal)
+refs.closeModalBtn.addEventListener('click', onCloseModal)
 refs.overley.addEventListener('click', onBackdropClick)
 
-function onOpenOriginalImage(event) {
+function onOpenModal(event) {
     event.preventDefault();
     
     if (event.target.nodeName !== 'IMG') {
         return;
     }
     refs.opneModal.classList.add("is-open")
-    const originalImg = event.target.dataset.source;
-    const imageAlt = event.target.getAtribute("alt");
+    window.addEventListener('keydown', onEscPress)
+    // const originalImg = event.target.dataset.source;
+    // const imageAlt = event.target.getAtribute("alt");
 
-    if(refs.opneModal.closest("is-open")){
-        openImageInModal(originalImg, imageAlt)
-    }    
+    // if(refs.opneModal.closest("is-open")){
+    //     openImageInModal(originalImg, imageAlt)
+    // }    
 }
 
-function keydownModuleWindow(event) {
-  if (event.code === 'Escape') {
-    return closeImageModal();
-  };
-};
 
 // function openImageInModal(image, alt, index) {
-    
-//     refs.imageEl.src = image;
-//     refs.imageEl.alt = alt;
-//     refs.imageEl.dataset.index = index;
-//     window.addEventListener('keydown', event => keydownModuleWindow(event));
-// }
-
-function closeImageModal() {
-    window.removeEventListener('keydown', keydownModuleWindow);
+  
+  //     refs.imageEl.src = image;
+  //     refs.imageEl.alt = alt;
+  //     refs.imageEl.dataset.index = index;
+  // }
+  
+  function onCloseModal() {
+    window.removeEventListener('keydown', onEscPress);
     refs.opneModal.classList.remove("is-open");
-    openImageInModal();
+};
+  
+  function onEscPress(event) {
+    const ESC_KEY_CODE = "Escape";
+    if (event.code === ESC_KEY_CODE) {
+      onCloseModal();
+    };
+};
+  
+  function onBackdropClick(event) {
+    if (event.currentTarget === event.target) {
+      onCloseModal()
+  }
 }
-function onBackdropClick(event) {
-    if (event.target === event.currentTarget) {
-        closeImageModal()
-    }
 
 // function onActivIndex(event) {
 //      let currentImg = evt.target.getAttribute("href");
 //      return images.findIndex((image) => currentImg === image.original);    
 // }
-function  openImageInModal(src = "", alt = "") {
-  refs.imageEl.alt = alt;
-  refs.imageEl = src;
-}
+// function  openImageInModal(src = "", alt = "") {
+//   refs.imageEl.alt = alt;
+//   refs.imageEl = src;
+// }
